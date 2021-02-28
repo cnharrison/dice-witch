4fea974a31a2b0aa2403cd9ffc0341ddd7dccefc
Christopher Harrison <cnharrison@gmail.com>
2021-02-27 21:37:08 -0500

    typo

--- src/controllers/sendNeedPermissionMessage.js
+++ src/controllers/sendNeedPermissionMessage.js
@@ -4,12 +4,7 @@ const sendNeedPermissionMessage = (message, logOutputChannel) => {
   message.channel.send(
     `doesn't look like i have permission to **attach files** in this channel. i need them to show you the dice 😅`
   );
-  const embed = new Discord.MessageEmbed()
-    .setColor("#FF0000")
-    .setDescription(
-      `**sent permissions request to ${message.channel} on ${message.guild}**`
-    );
-  logOutputChannel.send(embed).catch((err) => console.error(err));
+  logEvent("sentNeedPermissionsMessage", logOutputChannel, message);
 };
 
 module.exports = sendNeedPermissionMessage;
