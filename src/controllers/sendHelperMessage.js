0e4439a9a6ef8467dc0c90af593fe57ab5662ad5
Christopher Harrison <cnharrison@gmail.com>
2021-04-11 17:45:33 -0400

    add knowledge base command, copy edits

--- src/controllers/sendHelperMessage.js
+++ src/controllers/sendHelperMessage.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const { inviteLink, supportServerLink } = require("../../config.json");
+const { prefix, inviteLink, supportServerLink } = require("../../config.json");
 const { availableDice, maxDice } = require("../constants");
 const { logEvent } = require("../services");
 
@@ -10,17 +10,17 @@ const sendHelperMessage = async (message, name, logOutputChannel, args) => {
       .addFields(
         {
           name: `Need help? 😅`,
-          value: `You need to put least one valid argument after the **!${name}** command.\nArguments must be in valid [dice notation](http://dmreference.com/MRD/Basics/The_Basics/Dice_Notation.htm).\nYou can roll any of these dice: **${availableDice
+          value: `You need to put least one valid argument after the **${prefix}${name}** command.\nArguments must be in valid [dice notation](http://dmreference.com/MRD/Basics/The_Basics/Dice_Notation.htm).\nYou can roll any of these dice: **${availableDice
             .map((dice) => `d${dice}`)
             .join(", ")}**.`,
         },
         {
           name: "Basic rolls",
-          value: `\`!${name} 1d20\`: roll one twenty sided die\n\`!${name} 1d20 1d12 1d8\`: roll one twenty-sided die, one twelve-sided die, and one eight-sided die\n\`!${name} 1d12+3 5d4\`: roll one twelve-sided die, adding three to the total, and five four-sided dice\n\nYou can also subtract\`-\`, multiply\`*\`, and divide\`/\` rolls.\nYou can roll up to **${maxDice}** dice at once 😈\n\n`,
+          value: `\`${prefix}${name} 1d20\`: roll one twenty sided die\n\`${prefix}${name} 1d20 1d12 1d8\`: roll one twenty-sided die, one twelve-sided die, and one eight-sided die\n\`${prefix}${name} 1d12+3 5d4\`: roll one twelve-sided die, adding three to the total, and five four-sided dice\n\nYou can also subtract\`-\`, multiply\`*\`, and divide\`/\` rolls.\nYou can roll up to **${maxDice}** dice at once 😈\n\n`,
         },
         {
           name: "Advanced rolls and modifiers",
-          value: `Type \`!kb <topic>\` for explanations and examples:\n\nMin/Max: \`!kb minmax\`\nExploding 💥: \`!kb exploding\`\nRe-roll ♻: \`!kb reroll\`\nKeep/drop AKA Advantage: \`!kb keepdrop\`\nTarget success/failure 🎯: \`!kb target\`\nCritical success/failure ⚔: \`!kb crit\`\nSorting: \`!kb sort\`\nCompare point: \`!kb conmpare\`\nMath 🧮: \`!kb math\`\nGroup rolls: \`!kb groups\`\nModifier icon legend: \`!kb legend\``,
+          value: `Type \`${prefix}kb <topic>\` for explanations and examples:\n\nMin/Max: \`${prefix}kb minmax\`\nExploding 💥: \`${prefix}kb exploding\`\nRe-roll ♻: \`${prefix}kb reroll\`\nKeep/drop AKA Advantage: \`${prefix}kb keepdrop\`\nTarget success/failure 🎯: \`${prefix}kb target\`\nCritical success/failure ⚔: \`${prefix}kb crit\`\nSorting ↕: \`${prefix}kb sort\`\nMath 🧮: \`${prefix}kb math\`\nModifier icon legend: \`${prefix}kb legend\``,
         }
       )
       .addField(
