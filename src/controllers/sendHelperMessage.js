e594380d4389029dc51e8ace7364e10ca788ee40
Christopher Harrison <cnharrison@gmail.com>
2021-04-11 18:03:54 -0400

    helper message tweak

--- src/controllers/sendHelperMessage.js
+++ src/controllers/sendHelperMessage.js
@@ -12,11 +12,13 @@ const sendHelperMessage = async (message, name, logOutputChannel, args) => {
           name: `Need help? 😅`,
           value: `You need to put least one valid argument after the **${prefix}${name}** command.\nArguments must be in valid [dice notation](http://dmreference.com/MRD/Basics/The_Basics/Dice_Notation.htm).\nYou can roll any of these dice: **${availableDice
             .map((dice) => `d${dice}`)
-            .join(", ")}**.`,
+            .join(
+              ", "
+            )}**.\nYou can roll up to **${maxDice}** dice at once 😈\n\n`,
         },
         {
           name: "Basic rolls",
-          value: `\`${prefix}${name} 1d20\`: roll one twenty sided die\n\`${prefix}${name} 1d20 1d12 1d8\`: roll one twenty-sided die, one twelve-sided die, and one eight-sided die\n\`${prefix}${name} 1d12+3 5d4\`: roll one twelve-sided die, adding three to the total, and five four-sided dice\n\nYou can also subtract\`-\`, multiply\`*\`, and divide\`/\` rolls.\nYou can roll up to **${maxDice}** dice at once 😈\n\n`,
+          value: `\`${prefix}${name} 1d20\`: roll one twenty sided die\n\`${prefix}${name} 1d20 1d12 1d8\`: roll one twenty-sided die, one twelve-sided die, and one eight-sided die\n\`${prefix}${name} 1d12+3 5d4\`: roll one twelve-sided die, adding three to the total, and five four-sided dice\n\`!roll 3d6+3d6\`: Roll two sets of three six-sided dice and add the total.\n\n`,
         },
         {
           name: "Advanced rolls and modifiers",
