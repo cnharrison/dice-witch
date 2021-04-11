82ea31334942db8f73ab162c6b98d7cd05e4a988
Christopher Harrison <cnharrison@gmail.com>
2021-04-11 14:04:19 -0400

    update readme and helperMessage

--- src/controllers/sendHelperMessage.js
+++ src/controllers/sendHelperMessage.js
@@ -19,8 +19,8 @@ const sendHelperMessage = async (message, name, logOutputChannel, args) => {
           value: `\`!${name} 1d20\`: roll one twenty sided die\n\`!${name} 1d20 1d12 1d8\`: roll one twenty-sided die, one twelve-sided die, and one eight-sided die\n\`!${name} 1d12+3 5d4\`: roll one twelve-sided die, adding three to the total, and five four-sided dice\n\nYou can also subtract\`-\`, multiply\`*\`, and divide\`/\` rolls.\nYou can roll up to **${maxDice}** dice at once 😈\n\n`,
         },
         {
-          name: "Drop/keep dice AKA advantage rolls",
-          value: `\`!${name} 3d20b2\`: roll three twenty-sided dice and keep the best two\n\`!${name} 5d8w1\`: roll five eight-sided dice and keep the worst one`,
+          name: "Advanced rolls and modifiers",
+          value: `Type \`!kb <topic>\` for explanations and examples:\n\nMin/Max: \`!kb minmax\`\nExploding 💥: \`!kb exploding\`\nRe-roll ♻: \`!kb reroll\`\nKeep/drop AKA Advantage: \`!kb keepdrop\`\nTarget success/failure 🎯: \`!kb target\`\nCritical success/failure ⚔: \`!kb crit\`\nSorting: \`!kb sort\`\nCompare point: \`!kb conmpare\`\nMath 🧮: \`!kb math\`\nGroup rolls: \`!kb groups\`\nModifier icon legend: \`!kb legend\``,
         }
       )
       .addField(
