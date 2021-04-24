5c208679b9954845d14be00e911e653012f39aaa
Christopher Harrison <cnharrison@gmail.com>
2021-04-24 13:56:01 -0400

    filter d100 from args

--- src/services/rollDice.js
+++ src/services/rollDice.js
@@ -3,29 +3,29 @@ const { DiceRoll, Parser } = require("rpg-dice-roller");
 function generateIconArray(modifierSet) {
   return modifierSet.size > 0
     ? [...modifierSet].map((item) => {
-        switch (item) {
-          case "drop":
-            return "trashcan";
-          case "explode":
-            return "explosion";
-          case "re-roll":
-            return "recycle";
-          case "max":
-            return "chevronUp";
-          case "min":
-            return "chevronDown";
-          case "target-success":
-            return "bullseye";
-          case "critical-success":
-            return "crit";
-          case "critical-failure":
-            return "dizzyFace";
-          case "penetrate":
-            return "arrowThrough";
-          default:
-            return "blank";
-        }
-      })
+      switch (item) {
+        case "drop":
+          return "trashcan";
+        case "explode":
+          return "explosion";
+        case "re-roll":
+          return "recycle";
+        case "max":
+          return "chevronUp";
+        case "min":
+          return "chevronDown";
+        case "target-success":
+          return "bullseye";
+        case "critical-success":
+          return "crit";
+        case "critical-failure":
+          return "dizzyFace";
+        case "penetrate":
+          return "arrowThrough";
+        default:
+          return "blank";
+      }
+    })
     : null;
 }
 
@@ -38,14 +38,24 @@ const rollDice = (args, availableDice) => {
 
   try {
     argsToMutate.forEach((value, outerIndex) => {
+      let parsedRoll;
+      try {
+        parsedRoll = Parser.parse(value);
+      } catch (err) {
+        console.error(err);
+      }
       const isMultiRollToken = value.match(/^.*?(\<[^\d]*(\d+)[^\d]*\>).*/);
+      const deleteRoll = (args, outerIndex) =>
+        args.filter((_, index) => index !== outerIndex);
       if (isMultiRollToken) {
         const number = Number(isMultiRollToken[2]);
-        argsToMutate = argsToMutate.filter((_, index) => index !== outerIndex);
+        argsToMutate = deleteRoll(argsToMutate, outerIndex);
         argsToMutate = new Array(number).fill(argsToMutate).flat();
       }
+      if (parsedRoll && parsedRoll[0].sides === 100) {
+        argsToMutate = deleteRoll(argsToMutate, outerIndex);
+      }
     });
-
     argsToMutate.forEach((value) => {
       let parsedRoll;
 
@@ -57,9 +67,9 @@ const rollDice = (args, availableDice) => {
 
       const sidesArray = parsedRoll
         ? parsedRoll
-            .filter((rollGroup) => typeof rollGroup !== "string")
-            .filter((rollGroup) => typeof rollGroup !== "number")
-            .map((roll) => roll.sides)
+          .filter((rollGroup) => typeof rollGroup !== "string")
+          .filter((rollGroup) => typeof rollGroup !== "number")
+          .map((roll) => roll.sides)
         : [];
 
       const isValid =
