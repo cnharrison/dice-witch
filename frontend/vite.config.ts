d094d40a5377b944e60783f52428bdb763a3f704
Christopher Harrison <cnharrison@gmail.com>
2025-03-01 15:19:03 -0500

    fix: embed proxy target at build time?

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,27 +1,22 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
-import { defineConfig, loadEnv } from "vite"
+import { defineConfig } from "vite"
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  console.log('Loaded env variables:', env)
-
-  return {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    server: {
-      port: parseInt(env.VITE_PORT || "5173"),
-      proxy: {
-        '/api': {
-          target: env.VITE_API_PROXY_TARGET || 'http://localhost:3000',
-          changeOrigin: true,
-          secure: false,
-        }
-      },
+  },
+  server: {
+    port: parseInt(process.env.VITE_PORT || "5173"),
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_PROXY_TARGET || 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+      }
     },
-  }
+  },
 })
