f16b0278c7f6f0c61e137e801662f804895f4d26
Christopher Harrison <cnharrison@gmail.com>
2025-03-01 13:27:27 -0500

    chore: edit scripts and viteconfig, rm Dockerfile

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,27 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  console.log('Loaded env variables:', env)
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  server: {
-    port: parseInt(process.env.VITE_PORT || "5173"),
-    proxy: {
-      '/api': {
-        target: process.env.VITE_API_PROXY_TARGET || 'http://localhost:3000',
-        changeOrigin: true,
-        secure: false,
-      }
+    server: {
+      port: parseInt(env.VITE_PORT || "5173"),
+      proxy: {
+        '/api': {
+          target: env.VITE_API_PROXY_TARGET || 'http://localhost:3000',
+          changeOrigin: true,
+          secure: false,
+        }
+      },
     },
-  },
+  }
 })
